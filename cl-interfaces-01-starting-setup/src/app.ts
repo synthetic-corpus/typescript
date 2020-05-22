d140118d40be5223d55afb69b0d9dc56609d7671
Joel <joel@joelgonzaga.com>
2020-05-22 12:19:53 -0700

    created new notes with typescript.

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,8 +1,9 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
-
+  // private employees: string[] = [];
+  /* protected Variables are variables that can be accessed by an extended class */
+  protected employees: string[] = []; 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
