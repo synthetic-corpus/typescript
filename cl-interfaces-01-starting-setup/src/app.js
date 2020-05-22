b40783229d3a82509a6fbd9602cc0d60cb773a69
Joel <joel@joelgonzaga.com>
2020-05-22 10:57:41 -0700

    synced with instructor files.

--- cl-interfaces-01-starting-setup/src/app.js
+++ cl-interfaces-01-starting-setup/src/app.js
@@ -1,12 +1,23 @@
-// Code goes here!
 var Department = /** @class */ (function () {
-    function Department(n) {
-        this.name = n;
+    function Department(name, id) {
+        this.name = name;
+        this.id = id;
+        this.employees = [];
+        // When you make the constructor like this, you don't have to repeat things.
     }
     Department.prototype.describe = function () {
-        console.log("The Department is " + this.name);
+        console.log("The Department is " + this.name + ". Has and ID of " + this.id);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.getEmployees = function () {
+        console.log(this.employees);
     };
     return Department;
 }());
-var dept = new Department('Biology');
+var dept = new Department('Biology', '13');
+dept.addEmployee(['ted', 'danza', 14]);
+dept.addEmployee(['brick', 'top', 09]);
 dept.describe();
+dept.getEmployees();
--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,15 +1,70 @@
-// Code goes here!
 class Department {
-    name: String;
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-    constructor(n: string) {
-        this.name = n
-    }
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
 
-    describe() {
-        console.log(`The Department is ${this.name}`)
-    }
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    // validation etc
+    // this.id = 'd2';
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-const dept = new Department('Biology');
-dept.describe()
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('d1', ['Max']);
+
+it.addEmployee('Max');
+it.addEmployee('Manu');
+
+// it.employees[2] = 'Anna';
+
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.addReport('Something went wrong...');
+
+accounting.printReports();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
