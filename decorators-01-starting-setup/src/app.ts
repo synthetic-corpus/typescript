3168b4ab8fcb504fcab7b695e5b810b3f94aa292
Joel <joel@joelgonzaga.com>
2020-06-21 13:05:54 -0700

    Example given with a 'Factory' function decorator.

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -1,12 +1,16 @@
-function Logger(target: Function){
-    console.log('Logging!');
-    console.log(target)
+function Logger(log: String){
+    //
+    return function(target: Function){
+        // This function still responds to the Target below
+        console.log(log)
+        console.log(target)
+    }
 }
 
 // @ is a special Identifier
 // @ points at a funtion, which is the decorater
 // The decorator requires an arguement
-@Logger
+@Logger('I am about to log...')
 class Person {
     name = 'Max';
 
